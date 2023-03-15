22abeb1aecfc1d0f1c65becd40fbf0092d9c14c1
Yash Hegde <yash4success@gmail.com>
2023-03-16 00:58:29 +0530

    email+url+names+sentence search

--- regex_scripts.py
+++ regex_scripts.py
@@ -7,8 +7,30 @@ random@work.net
 
 '''
 
-pattern = re.compile(r'[a-zA-Z0-9.-+_]+@[a-zA-Z-0-9]+\.(com|net|ac.in)')
-matches = pattern.finditer(emails)
+urls = '''
+https://www.google.com
+http://coreyms.com
+https://youtube.com
+https://www.nasa.gov
+'''
+names = '''
+Mr. Yash
+Mr. Yasg
+Mr Yashi
+Mr. Yashu
+'''
+
+#pattern = re.compile(r'[a-zA-Z0-9.-+_]+@[a-zA-Z-0-9]+\.(com|net|ac.in)')
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
+
+#matches = pattern.finditer(emails)
+matches = pattern.finditer(urls)
 
 for match in matches :
-    print(match)
+    print(match.group(3))
+
+
+sentence = 'Start a sentence and then bring it to an end'
+patterns = re.compile(r'sentence')
+matches1 = patterns.search(sentence)
+print(matches1)
